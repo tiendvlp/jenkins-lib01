34aac0dc83fa4ee5fbcf9e996ce4a08339e5fd8f
tiendvlp <tiendvlp@gmail.com>
2021-10-16 15:12:21 +0700

    a

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -4,7 +4,7 @@ import groovy.io.*;
 def call(Map config=[:]) {
     def dri = new File (pwd());
     
-    new File (dir.path + '\\releasenote.txt').withWriter('utf-8') {
+    new File (dir.path + '/releasenote.txt').withWriter('utf-8') {
         writer -> 
             dir.eachFileRecurse(FileType.ANY) { file -> 
                 if (file.isDirectory()) {
