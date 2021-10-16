9d431e35ac4d2f53660b1d150a6cc352b2e3743d
tiendvlp <tiendvlp@gmail.com>
2021-10-16 14:55:06 +0700

    init

---
+++ jenkins.groovy
@@ -0,0 +1,17 @@
+import java.io.*;
+import groovy.io.*;
+
+def call(Map config=[:]) {
+    def dri = new File (pwd());
+    
+    new File (dir.path + '\\releasenote.txt').withWriter('utf-8') {
+        writer -> 
+            dir.eachFileRecurse(FileType.ANY) { file -> 
+                if (file.isDirectory()) {
+                    writer.writeLine(file.name);
+                } else {
+                    writer.writeLine('\t' + file.name + '\t' + file.length());
+                }
+            }
+    }
+}
